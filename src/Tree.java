f60b35d7b6d71ec10fa06153c29be4b200a3986c
GalFom <50401817+GalFom@users.noreply.github.com>
2019-05-19 16:10:40 +0300

    Search item and add

--- src/Tree.java
+++ src/Tree.java
@@ -1,11 +1,60 @@
 public class Tree {
     private TreeNode root;
+    TreeNode newItem = new TreeNode();
+
 
     public void add(String data) {
-
+        if (root == null) {
+            newItem.data = data;
+            newItem.leftChild = null;
+            newItem.rightChild = null;
+            root = newItem;
+        } else {
+            if (newItem < root) { //
+                newItem = root.leftChild;
+                newItem.leftChild = null;
+                newItem.rightChild = null;
+            }
+            if (newItem < root.leftChild) {  //
+                root = root.leftChild;
+                newItem = root.leftChild;
+                newItem.leftChild = null;
+                newItem.rightChild = null;
+            }
+            if (newItem > root.leftChild && newItem < root.rightChild) {
+                root = root.leftChild;
+                newItem = root.rightChild;
+                newItem.leftChild = null;
+                newItem.rightChild = null;
+            }
+            f (newItem > root.rightChild) {  //
+                root = root.rightChild;
+                newItem = root.rightChild;
+                newItem.leftChild = null;
+                newItem.rightChild = null;
+            }
+        }
     }
 
     public boolean has(String data) {
-        return false;
+        if (root == null) {
+            return false;
+        }
+        if (data == root.data) {
+            return true;
+        }
+        else{
+            while (root.data!=data){
+                if(root.data < data) {
+                    root = root.leftChild;
+                }else {
+                    root = root.rightChild;
+                }
+            }
+            return true;
+        }
+
+
+
     }
 }
